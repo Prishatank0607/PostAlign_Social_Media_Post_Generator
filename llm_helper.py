6dee3a15aa903cef0a967b7b200a26c791e1cc42
Prisha Tank <104256520+Prishatank0607@users.noreply.github.com>
2025-04-07 13:01:45 +0530

    Update llm_helper.py
    Added NewsAPI

--- llm_helper.py
+++ llm_helper.py
@@ -4,7 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.1-8b-instant")
-
+news_api_key = os.getenv("NEWS_API_KEY")  # Fetch NewsAPI key from environment variables
+NEWS_API_URL = f"https://newsapi.org/v2/top-headlines?country=us&apiKey={news_api_key}"
 
 if __name__ == "__main__":
     response = llm.invoke("How pets understand humans? ")
